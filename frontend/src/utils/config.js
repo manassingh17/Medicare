afefcd29fec420faeb1b5797971a07d029789209
Manas Singh <manassingh17102003@gmail.com>
2025-04-01 18:44:00 +0530

    hosted backend

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-export const URL="http://localhost:5000"
-// export const URL="https://doctor-appointment-sd6u.onrender.com"
+// export const URL="http://localhost:5000"
+export const URL="https://medicare-1dlp.onrender.com/"
 export const cloudinary_upload_preset="song_upload"
 export const cloud_name="dtdfdhwj2"
